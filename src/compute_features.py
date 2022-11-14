69e6d9505968e7b3592dc79c33ed7fc8d0d883a9
Pablo Huijse <pablo.huijse@gmail.com>
2022-11-14 15:22:23 -0300

    Eliminar turbofats de los requisitos

--- src/compute_features.py
+++ src/compute_features.py
@@ -2,7 +2,6 @@ from pathlib import Path
 import yaml
 import pandas as pd
 import pickle
-import turbofats
 
 
 if __name__ == "__main__":
@@ -12,14 +11,12 @@ if __name__ == "__main__":
 
     feature_db = []
     print(f"Computing {params['list']}")
-    calculator = turbofats.FeatureSpace(feature_list=params['list'])
 
     for file in Path('data').glob('*.pkl'):
         with open(file, 'rb') as f:
-            lc, label = pickle.load(f)
-            lc['oid'] = file.stem
-            lc.set_index('oid', inplace=True)
-        lc_features = calculator.calculate_features(lc)
-        lc_features['label'] = label
-        feature_db.append(lc_features)
+            data, label = pickle.load(f)
+        oid = file.stem
+        features = {}
+        features['label'] = label
+        feature_db.append(pd.DataFrame(features, index=oid))
     pd.concat(feature_db).to_csv(output_dir / 'feature_table.csv')

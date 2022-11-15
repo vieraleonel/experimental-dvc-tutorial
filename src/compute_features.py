8cb44098c51980dce3410661054a6004ebf08ae8
Pablo Huijse <pablo.huijse@gmail.com>
2022-11-15 14:09:31 -0300

    cálculo de media y stddev como features

--- src/compute_features.py
+++ src/compute_features.py
@@ -1,8 +1,13 @@
 from pathlib import Path
 import yaml
+import numpy as np
 import pandas as pd
 import pickle
 
+feature_processor = {
+        'Mean': lambda data: np.mean(data['magnitude'].values),
+        'Std': lambda data: np.std(data['magnitude'].values)
+        }
 
 if __name__ == "__main__":
     params = yaml.safe_load(open("params.yaml"))["features"]
@@ -15,8 +20,11 @@ if __name__ == "__main__":
     for file in Path('data').glob('*.pkl'):
         with open(file, 'rb') as f:
             data, label = pickle.load(f)
-        oid = file.stem
-        features = {}
-        features['label'] = label
-        feature_db.append(pd.DataFrame(features, index=oid))
-    pd.concat(feature_db).to_csv(output_dir / 'feature_table.csv')
+        feature_row = {}
+        feature_row['oid'] = file.stem
+        feature_row['label'] = label
+        for feature_name in params['list']:
+            feature_row[feature_name] = feature_processor[feature_name](data)
+        feature_db.append(feature_row)
+    feature_db = pd.DataFrame(feature_db).set_index('oid')
+    feature_db.to_csv(output_dir / 'feature_table.csv')
